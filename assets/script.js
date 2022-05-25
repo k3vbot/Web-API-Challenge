4ec9be2496db294b10f3e2daa7333ead5308ec46
Kevin <muehl004@gmail.com>
2022-05-25 16:35:25 -0700

    added stylings for buttons

--- assets/script.js
+++ assets/script.js
@@ -1,61 +1,27 @@
-const quizContainer = document.getElementById('quiz')
-const resultsContainer = document.getElementById('results')
-const submitButton = document.getElementById('submit')
-const quizQuestions = [
-    {
-        question: "This is a question?",
-        answers: {
-            a: "This is an answer!",
-            b: "This is an answer!",
-            c: "This is an answer!"
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: "This is a question?",
-        answers: {
-            a: "This is an answer!",
-            b: "This is an answer!",
-            c: "This is an answer!"
-        },
-        correctAnswer: "a"
-    },
-    {
-        question: "This is a question?",
-        answers: {
-            a: "This is an answer!",
-            b: "This is an answer!",
-            c: "This is an answer!"
-        },
-        correctAnswer: "a"
-    },
-]
-
 function buildQuiz(){
     const output = [];
     quizQuestions.forEach(
         (currentQuestion, questionNumber) => {
             const answers = [];
             for(letter in currentQuestion.answers){
-                answers.push(
-                    `<label>
-                    <input type="radio" name="questions${questionNumber}" value="${letter}">
-                    ${letter} :
-                    ${currentQuestion.answers[letter]}
-                    </label>`
-                );
-            }
+            answers.push(
+                `<label>
+                <input type="radio" name="question${questionNumber}" value="${letter}">
+                ${letter} :
+                ${currentQuestion.answers[letter]}
+                </label>`
+            );
+        }
             output.push(
-                `<div class="questions"> ${currentQuestion.question} </div>
-                <div class="answers"> ${answers.join('')} </div>`
+                `<div class="slide">
+                    <div class="question"> ${currentQuestion.question} </div>
+                    <div class="answers"> ${answers.join('')} </div>
+                </div>`
             );
         }
     );
     quizContainer.innerHTML = output.join('');
 };
-quizQuestions.forEach( (currentQuestion, questionNumber) => {
-
-});
 
 function showResults(){
     const answerContainers = quizContainer.querySelectorAll('.answers');
@@ -64,10 +30,9 @@ function showResults(){
         const answerContainer = answerContainers[questionNumber];
         const selector = `input[name=question${questionNumber}]:checked`;
         const userAnswer = (answerContainer.querySelector(selector) || {}).value;
-        if(userAnswer ===currentQuestion.correctAnswer){
+        if(userAnswer === currentQuestion.correctAnswer){
             numCorrect++;
-            answerContainers[questionNumber].getElementsByClassName.color = 'lightgreen';
-        
+            answerContainers[questionNumber].style.color = 'green';
         }
         else{
             answerContainers[questionNumber].style.color = 'red';
@@ -75,6 +40,38 @@ function showResults(){
     });
     resultsContainer.innerHTML = `${numCorrect} out of ${quizQuestions.length}`;
 }
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+const quizQuestions = [
+    {
+        question: "This is a question?",
+        answers: {
+            a: "This is an answer!",
+            b: "This is not an answer!",
+            c: "This is definately an answer!"
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "Is this is a question?",
+        answers: {
+            a: "This isn't an answer!",
+            b: "This is an answer!",
+            c: "This is not an answer!"
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "This is definitely a question?",
+        answers: {
+            a: "This is not an answer!",
+            b: "This is definitely not an answer!",
+            c: "This is an answer!"
+        },
+        correctAnswer: "c"
+    }
+];
 
 
 
@@ -83,3 +80,4 @@ buildQuiz();
 
 
 submitButton.addEventListener('click', showResults);
+
